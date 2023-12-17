9070b5d47945d3e189831a35776c4050bbef0a2f
Gupta_ Rajeshwari (rajeshwgupta@deloitte.com) <rajeshwgupta@your.hostname.com>
2023-12-18 00:04:34 +0530

    sidenav

--- src/App.js
+++ src/App.js
@@ -3,17 +3,18 @@ import Explore from "./component/Explore";
 import Learning from "./component/Learning";
 import NavBar from "./component/NavBar";
 import Recommended from "./component/Recommended";
-
+import SideBar from "./component/SideBar";
 function App() {
   return (
     <>
       <div className="app">
         <NavBar />
-      </div>
-      <div className="content">
-        <Explore />
-        <Learning />
-        <Recommended />
+        <SideBar />
+        <div className="main-container">
+          <Explore />
+          <Learning />
+          <Recommended />
+        </div>
       </div>
     </>
   );
--- src/component/SideBar.js
+++ src/component/SideBar.js
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SideBar.css";
 
 export default function SideBar() {
+  const [isExpanded, setExpanded] = useState(false);
+  const toggleSideBar = () => {
+    setExpanded(!isExpanded);
+  };
   return (
     <>
-      <div className="sidebar">
-        <div>
-          <i className="fa-regular fa-bell"></i>
+      <div className={`side-navbar ${isExpanded ? "expanded" : ""}`}>
+        <div className="icon" onClick={toggleSideBar}>
+          <img src="./asset/Home.svg" alt="home" /> {isExpanded && <div className="learning">Home</div>}
+          <img src="./asset/Book.svg" alt="book" />{isExpanded && <div className="learning">My Learning</div>}
+          <img src="./asset/settings.svg" alt="setting" />{isExpanded && <div className="learning">Setting</div>}
         </div>
-        <i className="fa-regular fa-bell"></i>
       </div>
     </>
   );
